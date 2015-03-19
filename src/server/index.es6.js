30b35c583f172a6bd5d2715d983449ecbd100274
Sébastien Robaszkiewicz <sebastien.robaszkiewicz@gmail.com>
2015-03-19 11:25:37 +0100

    enter and exit methods in ServerPerformance

--- src/server/index.es6.js
+++ src/server/index.es6.js
@@ -16,19 +16,40 @@ class MyPerformance extends serverSide.Performance {
     super();
   }
 
-  connect(client) {
-    super.connect(client); // don't forget this
-    
-    client.receive('performance:start', () => { // when the client joins the performance...
-      client.send('performance:play'); // ... send a WebSocket message instruction to the client
-    });
-  }
+  /* If anything needs to happen when a client connects to the server,
+   * write it in the 'connect' method.
+   */
+  // connect(client) {
+  //   super.connect(client); // don't forget this
+
+  //   ... // your code
+  // }
 
   /* If anything needs to happen when a client disconnects from the server,
-   * write it in the disconnect method.
+   * write it in the 'disconnect' method.
    */
   // disconnect(client) {
+  //   ... // your code
+  //
   //   super.disconnect(client); // don't forget this
+  // }
+
+  /* If anything needs to happen when a client starts the performance,
+   * write it in the 'enter' method.
+   */
+  enter(client) {
+    super.enter(client);
+
+    client.send('performance:play');
+  }
+
+  /* If anything needs to happen when a client leaves the performance,
+   * write it in the 'exit' method.
+   */
+  // exit(client) {
+  //   ... // your code
+  //
+  //   super.exit(client); // don't forget this
   // }
 }
 
