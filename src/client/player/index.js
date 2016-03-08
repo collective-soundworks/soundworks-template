d36bfd146bb6a0720f72917132fc962c4d97f076
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-03-08 11:52:39 +0100

    cleaned test code

--- src/client/player/index.js
+++ src/client/player/index.js
@@ -1,12 +1,12 @@
 // import soundworks library (client side) and custom experience
 import soundworks from 'soundworks/client';
 import PlayerExperience from './PlayerExperience.js';
+
 // files to load
 const audioFiles = ['sounds/sound-welcome.mp3', 'sounds/sound-others.mp3'];
-// window.localStorage.debug = '';
 
 const init = () => {
-  // configuration shared by the server (cf. `views/default.ejs`)
+  // configuration options shared by the server (cf. `views/default.ejs`)
   const socketIO = window.CONFIG && window.CONFIG.SOCKET_CONFIG;
   const appName = window.CONFIG && window.CONFIG.APP_NAME;
 
