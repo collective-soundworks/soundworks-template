6b9a48daef4c6a5818a406b64276465df49e15f2
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-03-10 12:35:02 +0100

    updated comments

--- src/server/index.js
+++ src/server/index.js
@@ -5,20 +5,18 @@ import 'source-map-support/register';
 import * as soundworks from 'soundworks/server';
 import PlayerExperience from './PlayerExperience';
 
-
+// Initialize application with configuration options.
 soundworks.server.init({ appName: 'Template' });
 
-// Configure the services and create the experience.
+// Create the experience activity.
 //
 // Activities must be mapped to client types:
 // - the `'player'` clients (who take part in the scenario by connecting to the
-//   server through the root URL) need to communicate with the `checkin` and the
-//   `performance` on the server side;
+//   server through the root URL) need to communicate with the `checkin` (see
+// `src/server/PlayerExperience.js`) and the server side `PlayerExperience`.
 // - we could also map activities to additional client types (thus defining a
 //   route (url) of the from: `/${clientType}`
-
 const experience = new PlayerExperience('player');
-experience.require('shared-config');
 
-// Start the application with a given name.
+// Start the application.
 soundworks.server.start();
