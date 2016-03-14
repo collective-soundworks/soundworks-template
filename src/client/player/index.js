9bdcd6d44ea8b31b8be9a0e620e4ac3d4570f253
Norbert Schnell <Norbert.Schnell@ircam.fr>
2016-03-14 15:47:57 +0100

    doc in progress

--- src/client/player/index.js
+++ src/client/player/index.js
@@ -1,22 +1,25 @@
-// import soundworks library (client side)
+// import soundworks client side
 import * as soundworks from 'soundworks/client';
 
 // import player experience
 import PlayerExperience from './PlayerExperience.js';
 
-// files to load
-const files = ['sounds/sound-welcome.mp3', 'sounds/sound-others.mp3'];
+// list of files to load (passed to the experience)
+const files = [
+  'sounds/sound-welcome.mp3',
+  'sounds/sound-others.mp3'
+];
 
 // launch application when document is fully loaded
 window.addEventListener('load', () => {
-  // mandatory configuration options shared by the server through the html/default.ejs template
+  // mandatory configuration options received from the server through the html/default.ejs template
   const socketIO = window.CONFIG && window.CONFIG.SOCKET_CONFIG;
   const appName = window.CONFIG && window.CONFIG.APP_NAME;
 
   // initialize the 'player' client
   soundworks.client.init('player', { socketIO, appName });
 
-  // create player experience
+  // create client side (player) experience
   const experience = new PlayerExperience(files);
 
   // start the client
