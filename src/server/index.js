1d1932944c4a46a8667c6a4452dc5f586c115eab
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-05-10 19:31:27 +0200

    cosmetics

--- src/server/index.js
+++ src/server/index.js
@@ -1,22 +1,20 @@
-// Enable sourceMaps in node
+// enable sourcemaps in node
 import 'source-map-support/register';
-
-// Import Soundworks library (server side) and server side experience
+// import soundworks library (server side) and server side experience
 import * as soundworks from 'soundworks/server';
 import PlayerExperience from './PlayerExperience';
 
-// Initialize application with configuration options.
+// initialize application with configuration options
 soundworks.server.init({ appName: 'Template' });
 
-// Create the experience activity.
-//
-// Activities must be mapped to client types:
+// create the experience
+// activities must be mapped to client types:
 // - the `'player'` clients (who take part in the scenario by connecting to the
-//   server through the root URL) need to communicate with the `checkin` (see
-// `src/server/PlayerExperience.js`) and the server side `PlayerExperience`.
+//   server through the root url) need to communicate with the `checkin` (see
+// `src/server/playerExperience.js`) and the server side `playerExperience`.
 // - we could also map activities to additional client types (thus defining a
-//   route (url) of the from: `/${clientType}`
+//   route (url) of the following form: `/${clientType}`)
 const experience = new PlayerExperience('player');
 
-// Start the application.
+// start application
 soundworks.server.start();
