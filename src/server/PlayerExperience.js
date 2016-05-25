caa4fc53639f4ad0b27c81d1efa1500913918562
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-05-25 14:14:28 +0200

    cosmetics

--- src/server/PlayerExperience.js
+++ src/server/PlayerExperience.js
@@ -1,27 +1,18 @@
-// Import Soundworks server side Experience
 import { Experience } from 'soundworks/server';
 
-/**
- * Server-side 'player' experience.
- */
+// server-side 'player' experience.
 export default class PlayerExperience extends Experience {
-  /**
-   *
-   */
   constructor(clientType) {
     super(clientType);
 
     this.checkin = this.require('checkin');
   }
 
-  /**
-   * If anything needs to happen when a client enters the performance (*i.e.*
-   * starts the experience on the client side), write it in the `enter`
-   * method.
-   */
+  // if anything needs to happen when a client enters the performance (*i.e.*
+  // starts the experience on the client side), write it in the `enter` method
   enter(client) {
     super.enter(client);
-    // Send a message to all the other clients of the same type
+    // send a message to all the other clients of the same type
     this.broadcast(client.type, client, 'play');
   }
 
@@ -29,24 +20,4 @@ export default class PlayerExperience extends Experience {
     super.exit(client);
     // ...
   }
-
-  /**
-   * If anything needs to happen when a client connects to the server,
-   * write it in the `connect` method.
-   */
-  // connect(client) {
-  //   super.connect(client); // don't forget this
-  //
-  //   ... // your code
-  // }
-
-  /**
-   * If anything needs to happen when a client disconnects from the server,
-   * write it in the `disconnect` method.
-   */
-  // disconnect(client) {
-  //   super.disconnect(client); // don't forget this
-  //
-  //   ... // your code
-  // }
 }
