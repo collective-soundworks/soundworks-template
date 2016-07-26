be45478642a63db059cf83f8f597a34c79d71ed8
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2016-07-26 11:00:36 +0200

    simplified client initialization

--- src/client/player/index.js
+++ src/client/player/index.js
@@ -12,17 +12,17 @@ const files = [
 
 // launch application when document is fully loaded
 window.addEventListener('load', () => {
-  // configuration received from the server through the `index.html`
+  // initialize the client with configuration received
+  // from the server through the `index.html`
   // @see {~/src/server/index.js}
   // @see {~/html/default.ejs}
-  const { appName, clientType, socketIO, assetsDomain }  = window.soundworksConfig;
-  // initialize the 'player' client
-  soundworks.client.init(clientType, { appName, socketIO });
+  const config = window.soundworksConfig;
+  soundworks.client.init(config.clientType, config);
   soundworks.client.setViewContentDefinitions(viewContent);
   soundworks.client.setViewTemplateDefinitions(viewTemplates);
 
   // create client side (player) experience
-  const experience = new PlayerExperience(assetsDomain, files);
+  const experience = new PlayerExperience(config.assetsDomain, files);
 
   // start the client
   soundworks.client.start();
