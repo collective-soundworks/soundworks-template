547c5b33fd5c477ae72e2ca6bbe3f967da6620b8
Benjamin Matuszewski <benjamin.matuszewski@gmail.com>
2017-04-03 16:16:39 +0200

    updated config files handling

--- src/server/index.js
+++ src/server/index.js
@@ -1,14 +1,18 @@
 import 'source-map-support/register'; // enable sourcemaps in node
+import path from 'path';
 import * as soundworks from 'soundworks/server';
 import PlayerExperience from './PlayerExperience';
-import defaultConfig from './config/default';
 
+const configName = process.env.ENV || 'default';
+const configPath = path.join(__dirname, 'config', configName);
 let config = null;
 
-switch(process.env.ENV) {
-  default:
-    config = defaultConfig;
-    break;
+// rely on node require because is synchronous
+try {
+  config = require(configPath).default;
+} catch(err) {
+  console.error(`Invalid ENV "${configName}", file "${configPath}.js" not found`);
+  process.exit(1);
 }
 
 // configure express environment ('production' enables cache systems)
