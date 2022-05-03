3eeded5c6b3272d3c56267b0cd34526b93232da8
b-ma <benjamin.matuszewski@gmail.com>
2022-05-03 10:06:34 +0200

    added schema loader

--- src/server/schemas/globals.js
+++
@@ -1,6 +0,0 @@
-export default {
-  flag: {
-    type: 'boolean',
-    default: true,
-  }
-}
--- src/server/schemas/loader.js
+++ src/server/schemas/loader.js
@@ -1,3 +1,7 @@
 // note: we should be able to modify this file while still being able to safely
 // inject new schemas registrations programmatically
-export async function schemasLoader(server) {}
+export async function schemasLoader(server) {
+  // import and register new schemas following the boilerplate bellow
+  // const { default: globals } = await import('./globals.js');
+  // server.stateManager.registerSchema('globals', globals);
+}
